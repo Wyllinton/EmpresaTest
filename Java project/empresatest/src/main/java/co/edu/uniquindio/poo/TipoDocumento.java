699b5e30f0707971816abc57ae600e8c41e468c4
Willinton Vergara Cataño <willinton.vergarac@uqvirtual.edu.co>
2023-10-22 09:42:56 -0500

    Clase Tipo Documento

---
+++ Java project/empresatest/src/main/java/co/edu/uniquindio/poo/TipoDocumento.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.poo;
+
+public enum TipoDocumento {
+    CEDULA(0), PASAPORTE(1), CEDULA_EXTRANJERA(2);
+
+    private TipoDocumento(int num){
+        this.num = num;
+    }
+
+    public int getNum(){
+        return num;
+    }
+    
+    private Integer num;
+}
